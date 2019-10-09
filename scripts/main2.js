74fb1b29f40715eaa31858d5d2966b5596e51a30
alexmaiorov <alexeymajor0@gmail.com>
2019-10-09 18:23:57 +0300

    Выполнено дз 4

--- scripts/main2.js
+++ scripts/main2.js
@@ -1,16 +1,27 @@
 //Задание 2
-var basket = [
-    ['yokohama', 100],
-    ['triangle', 50],
-    ['toyo', 200]
-]
+const NAMES = ['Yokohama', 'TOYO', 'Triangle', 'Continental', 'Pirelli', 'Michelin', 'Nokian'];
+const PRICES = [6740, 7570, 4150, 9650, 8410, 10350, 4470];
 
-console.log('Задание 2\n' + 'Итоговая стоимость:' + countBasketPrice(basket));
-
-function countBasketPrice(arr){
-    var total = 0;
-    for(let i = 0; i < arr.length; i++) {
-        total += arr[i][1];
+let store = {
+    catalog: [],
+    cart: [],
+    sum: 0,
+    
+    createDTO: function(){
+        for(let i = 0; i < NAMES.length; i++){
+            this.catalog.push (this.createProduct(NAMES[i], PRICES[i], i));
+        }
+    },
+    createProduct: function(name, price, j){
+        return {
+            id: j+1,
+            name: name,
+            price: price
+        }
+    },
+    SumPrice: function(){
+        for (let el of this.catalog){
+            this.sum += el.price;
+        }
     }
-    return total;
 }
